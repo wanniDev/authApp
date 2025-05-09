ba03e56d62dd7b2704c5067aa3bb8c237ea45f54
Wanni <wannidev0928@gmail.com>
2025-05-09 22:42:33 +0900

    프로덕션 배포시 민감정보를 숨기기위해 동작 여부 확인후 설정 값 환경변수로 변경

--- src/main/java/me/spring/authapp/LoginController.java
+++ src/main/java/me/spring/authapp/LoginController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-//    @GetMapping("/login")
-//    public String login(){
-//        return "login Page";
-//    }
+    @GetMapping("/login")
+    public String login(){
+        return "login Page";
+    }
 }
