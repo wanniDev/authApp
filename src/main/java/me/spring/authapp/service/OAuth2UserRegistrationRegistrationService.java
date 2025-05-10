99048ded13aafd966add93d8b6571ec5be675fd5
Wanni <wannidev0928@gmail.com>
2025-05-11 00:14:35 +0900

    다양한 권한 서버 제공자에 권한 및 인증 요청을 수행하게될 서비스 코드 구현

---
+++ src/main/java/me/spring/authapp/service/OAuth2UserRegistrationRegistrationService.java
@@ -0,0 +1,28 @@
+package me.spring.authapp.service;
+
+import me.spring.authapp.model.ProviderUser;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OAuth2UserRegistrationRegistrationService extends BaseUserRegistrationService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        ClientRegistration clientRegistration = userRequest.getClientRegistration();
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = oAuth2UserService.loadUser(userRequest);
+
+        ProviderUser providerUser = super.providerUser(clientRegistration,oAuth2User);
+
+        super.register(providerUser, userRequest);
+
+        return oAuth2User;
+    }
+}
